a4a3550e60f58d5b5093565d364ca62985f0e22c
JohnhoJ1 <email [at] johnyonjan1234@gmail.com>
2020-02-23 17:42:28 +0545

    dashboard fragment created

---
+++ app/src/main/java/com/john/jmusicstore/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,42 @@
+package com.john.jmusicstore.ui.dashboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.john.jmusicstore.R;
+
+public class DashboardFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private Button btn_gyro, btn_acc, btn_proxi;
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+
+
+        return root;
+    }
+}
