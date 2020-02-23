a4160751d2ec70670ad1cd884d70ee57ba10f5dc
JohnhoJ1 <email [at] johnyonjan1234@gmail.com>
2020-02-23 17:47:45 +0545

    mesage fragment completed

---
+++ app/src/main/java/com/john/jmusicstore/ui/message/MessageFragment.java
@@ -0,0 +1,45 @@
+package com.john.jmusicstore.ui.message;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.john.jmusicstore.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MessageFragment extends Fragment {
+    private MapView mapView;
+
+
+    public MessageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_message, container, false);
+
+        mapView = v.findViewById(R.id.map);
+//        mapView.getMapAsync(this);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return v;
+    }
+
+}
