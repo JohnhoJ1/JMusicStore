2963b90a6569441e0b5ecf976bbfbb8656dae1ae
JohnhoJ1 <email [at] johnyonjan1234@gmail.com>
2020-02-23 17:38:15 +0545

    login bll class created

---
+++ app/src/main/java/com/john/jmusicstore/bll/LoginBLL.java
@@ -0,0 +1,16 @@
+package com.john.jmusicstore.bll;
+
+import com.john.jmusicstore.api.UserAPI;
+import com.john.jmusicstore.server_response.SignUpResponse;
+import com.john.jmusicstore.url.URL;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class LoginBLL {
+    boolean isSuccessful = false;
+
+
+}
