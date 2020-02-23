bf32c5edf28b8bff5a6aa859390930cbbc08c694
JohnhoJ1 <email [at] johnyonjan1234@gmail.com>
2020-02-23 17:35:47 +0545

    adding slider adapter complete

--- app/src/main/java/com/john/jmusicstore/adapter/sliderAdapter.java
+++ app/src/main/java/com/john/jmusicstore/adapter/sliderAdapter.java
@@ -32,5 +32,27 @@ public class sliderAdapter extends PagerAdapter {
         return view == object;
     }
 
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider, null);
+        ImageView imageView =  view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+
+    }
 
 }
